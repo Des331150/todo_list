31a5762283e5aecb1dc724a2d68fcd21317d6de2
Desmond Papa Arhinful <arhinfuldesmondpapa02@gmail.com>
2025-04-06 21:30:16 +0000

    so created the create function view and validation

--- todo_list/tasks/models.py
+++ todo_list/tasks/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+class Tasks(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    isCompleted = models.BooleanField(default=False)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+
--- todo_list/tasks/views.py
+++ todo_list/tasks/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect, HttpResponse
+from .models import Tasks
+from django.urls import reverse
+
 
 # Create your views here.
+def create_task(request):
+    if request.method == 'POST':
+        title = request.POST.get("title")
+        description = request.POST.get("description")
+        createdAt = request.POST.get("createdAt")
+        
+        if title and description and createdAt:
+            task = Tasks(title=title, description=description, createdAt=createdAt)
+            task.save()
+            return HttpResponseRedirect(reverse("success_page"))
+        else:
+            return HttpResponse("All fields are required.")
