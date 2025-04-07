a971013a830e99eba5b191db6321a1bf9e14d70a
Desmond Papa Arhinful <arhinfuldesmondpapa02@gmail.com>
2025-04-07 20:32:35 +0000

    wrote the create, update and delete views and add the urls.

--- todo_list/tasks/urls.py
+++ todo_list/tasks/urls.py
@@ -1,1 +1,10 @@
 from django.urls import path
+from . import views
+
+app_name = "tasks"
+urlpatterns = [
+    path('create_task/', views.create_task, name="create_task"),
+    path('update/<int:task_id/>', views.update_task, name="update_task"),
+    path('delete/<int:task_id>/', views.delete_task, name="delete_task"),
+    path('success_path/', views.success_path, name="success_path")
+]
--- todo_list/tasks/views.py
+++ todo_list/tasks/views.py
@@ -16,3 +16,33 @@ def create_task(request):
             return HttpResponseRedirect(reverse("success_page"))
         else:
             return HttpResponse("All fields are required.")
+        
+def success_path(request):
+    return HttpResponse("Success")
+
+def update_task(request, task_id):
+    task = Tasks.objects.get(pk=task_id)
+    task.title = new_title
+    task.description = new_description
+
+    if request.method == 'POST':
+        new_title = request.POST.get("new_title")
+        new_description = request.POST.get("new_description")
+
+        if new_title and new_description:
+            task = Tasks(new_title=new_title, new_description=new_description)
+            task.save()
+            return HttpResponse("Task updated successfully.")
+        else:
+            return HttpResponse("All fields must be filled.")
+        
+
+def delete_task(request, task_id):
+    if request.method == 'POST':
+        try:
+            task = Tasks.objects.get(pk=task_id)
+
+            task.delete()
+            return HttpResponse("Task deleted successfully.")
+        except Tasks.DoesNotExist:
+            return HttpResponse("Task does not exist!")
